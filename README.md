6eb11e7d776a3b2840310d1daffcf7eb1f85a61f
sutang01 <sutangqiuyinong@gmail.com>
2025-07-02 18:05:25 +0800

    更新依赖项，添加新的组件和样式，修改主页内容以使用按钮组件，新增侧边栏样式变量。

---
+++ README.md
@@ -0,0 +1,2 @@
+react version 15.1.6
+shadcnUi version 2.1.8 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button"; 
 
 export default function Home() {
   return (
     <div>
-      <p className="font-bold text-rose-500">
-        Hello World!
-      </p>
+      <Button variant="default">
+        Click Me
+      </Button>
     </div>
   );
 }
